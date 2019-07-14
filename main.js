0064ae0917c0e3bd23ba1bd71e9a86e7409c104f
Kayla <kaynuhe@gmail.com>
2019-07-14 17:18:30 -0400

    function: check for valid email submission

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 console.log("it's alive!");
 
 document.addEventListener('DOMContentLoaded', function() {
+    
     const flkty = new Flickity( '.carousel-container', {
     // options
     cellAlign: 'left',
     contain: true,
     });
 
+    // form validation
+
+    const subscriptionForm = document.querySelector('form');
+    
+    subscriptionForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let userEmail = document.querySelector('.email');
+        let test = userEmail.value.split('');
+       
+        if (
+            test.includes('@') &&
+            test.includes('.') &&
+            !test.includes(' ')
+        ) {
+            alert("Thanks for subscribing!")
+        }
+        else {
+            alert("Please enter a valid email address")
+        };
+
+
+
+    })
+
+    
+
     
 
 
-    
+
 
 
 });

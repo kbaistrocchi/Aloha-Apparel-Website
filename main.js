b2b62b6a01c0c7b97a0a0ec820511945caa998fd
Kayla <kaynuhe@gmail.com>
2019-07-14 13:39:02 -0400

    feat: main.js initialize flickity carousel - no styling

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("it's alive!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    const flkty = new Flickity( '.carousel-container', {
+    // options
+    cellAlign: 'left',
+    contain: true
+    });
+
+
+
+});
